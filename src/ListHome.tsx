8c84c232d551e7b45f10ffa1e5d3e075da8ab46c
quantumleeps <dan.leeper@gmail.com>
2019-02-12 15:47:11 -0500

    Refactor field loading -chain the proper promises -make the ability to update the view. needs pulldown

--- src/ListHome.tsx
+++ src/ListHome.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { UserInfo } from "react-adal";
 import { withRouter } from "react-router-dom";
-import Alert from "react-s-alert"
+import Alert from "react-s-alert";
 import styled from "styled-components";
 
 import { ItemAddResult, Web } from "@pnp/sp";
@@ -30,8 +30,8 @@ const BoxChild = styled.div`
   box-shadow: 1px 1px 6px #000000;
   border: ${props =>
     props.validityState === 1
-    ? "3px solid green"
-    : props.validityState === 2
+      ? "3px solid green"
+      : props.validityState === 2
       ? "3px solid red"
       : "none"};
 `;
@@ -42,6 +42,8 @@ const BoxTitle = styled.div`
   display: flex;
   justify-content: space-between;
 `;
+
+const opsWeb = new Web(endpoint + "/operations");
 
 const Listings = (props: any) => {
   const dataBoxes = props.datapoints.map((e: any, i: any) => (
@@ -108,51 +110,85 @@ class ListHome extends React.Component<IAppProps, IAppState> {
     this.handleDataInput = this.handleDataInput.bind(this);
     this.submitRecord = this.submitRecord.bind(this);
     this.refreshPage = this.refreshPage.bind(this);
+    this.handleButtonClickem = this.handleButtonClickem.bind(this);
+  }
+
+  //    this performs the first time a page loads or if
+  //    a new view is selected. When a new view is selected
+  //    the this.state.datapoints needs to be cleared out
+  // 1. get the default view's view Id ( getDefaultId() )
+  // 2. get the default view's internal fieldnames list ( getFieldList() )
+  // 3. for each item in the list, create an object and add ( populateDatapoints() )
+  //    it to the this.state.datapoints array
+
+  public getDefaultView(web: any, listId: string) {
+    return web.lists.getById(listId).defaultView.get();
+  }
+
+  public getViewField(web: any, listId: string, viewId: string) {
+    return web.lists
+      .getById(listId)
+      .views.getById(viewId)
+      .fields.get();
+  }
+
+  public getField(web: any, listId: string, fieldInternalName: any) {
+    return web.lists
+      .getById(listId)
+      .fields.getByInternalNameOrTitle(fieldInternalName)
+      .get();
+  }
+
+  public loadDatapointsToState(web: any, listId: string, viewId: string) {
+    this.getViewField(web, listId, viewId).then((viewField: any) => {
+      viewField.Items.forEach((fieldInternalName: any, index: number) => {
+        this.getField(
+          opsWeb,
+          this.props.match.params.id,
+          fieldInternalName
+        ).then((field: any) => {
+          const objectCopy = Object.assign({}, this.state);
+          const pushData = {
+            group: field.Description.length
+              ? JSON.parse(field.Description).group
+              : null,
+            id: field.Id,
+            internalName: field.EntityPropertyName,
+            max: field.MaximumValue,
+            min: field.MinimumValue,
+            name: field.Title,
+            order: index,
+            units: field.Description.length
+              ? JSON.parse(field.Description).units
+              : null,
+            validityState: 0,
+            value: ""
+          };
+          objectCopy.datapoints.push(pushData);
+          this.setState(objectCopy);
+        });
+      });
+    });
   }
 
   public componentWillMount() {
-    const web = new Web(endpoint + "/operations");
-    web.lists
-      .getById(this.props.match.params.id)
-      .defaultView
-      .fields.get()
-      .then((item: any) => {
-        if (item.length > 0) {
-          this.setState({ loading: false });
-        }
-        item.Items.forEach((field: any, index: number) => {
-          web.lists
-            .getById(this.props.match.params.id)
-            .fields.getByInternalNameOrTitle(field)
-            .get()
-            .then((thing: any) => {
-              if (thing.Title !== "datetime") {
-                const objectCopy = Object.assign({}, this.state);
-                const pushData = {
-                  id: thing.Id,
-                  internalName: thing.EntityPropertyName,
-                  max: thing.MaximumValue,
-                  min: thing.MinimumValue,
-                  name: thing.Title,
-                  order: index,
-                  units: thing.Description.length
-                    ? JSON.parse(thing.Description).units
-                    : null,
-                  validityState: 0,
-                  value: ""
-                };
-                objectCopy.datapoints.push(pushData);
-                this.setState(objectCopy);
-              }
-            });
-        });
-      });
-    web.lists
+    this.getDefaultView(opsWeb, this.props.match.params.id).then(
+      (view: any) => {
+        this.loadDatapointsToState(opsWeb, this.props.match.params.id, view.Id);
+      }
+    );
+
+    opsWeb.lists
       .getById(this.props.match.params.id)
       .get()
       .then((list: any) => this.setState({ curTitle: list.Title }));
     const thisUser: UserInfo = adalContext.GetUser();
     this.setState({ curUser: thisUser });
+
+    // opsWeb.lists.getById(this.props.match.params.id).views.get().then((views:any[]) => {
+    //   console.log(views)
+    //   // "5bf68c7a-b494-4487-b97f-74335c6ed8ac"
+    // })
   }
 
   public componentDidMount() {
@@ -180,18 +216,18 @@ class ListHome extends React.Component<IAppProps, IAppState> {
   }
 
   public updateFormValidity() {
-    let numValid = 0
-    let numInvalid = 0
-    this.state.datapoints.forEach((datapoint:any) => {
-      datapoint.validityState === 2 
-      ? numInvalid++
-      : numInvalid = numInvalid
-      datapoint.validityState === 1
-      ? numValid++
-      : numValid = numValid
-    })
-    numInvalid > 0 ? this.setState({ formIsValid: false }) : this.setState({ formIsValid: true })
-    numValid < 1 ? this.setState({ formIsValid: false }) : this.setState({ formIsValid: true })
+    let numValid = 0;
+    let numInvalid = 0;
+    this.state.datapoints.forEach((datapoint: any) => {
+      datapoint.validityState === 2 ? numInvalid++ : (numInvalid = numInvalid);
+      datapoint.validityState === 1 ? numValid++ : (numValid = numValid);
+    });
+    numInvalid > 0
+      ? this.setState({ formIsValid: false })
+      : this.setState({ formIsValid: true });
+    numValid < 1
+      ? this.setState({ formIsValid: false })
+      : this.setState({ formIsValid: true });
   }
 
   public handleDataInput(event: any) {
@@ -213,6 +249,11 @@ class ListHome extends React.Component<IAppProps, IAppState> {
     });
   }
 
+  public newView(viewId:string) {
+    this.setState({ datapoints: [] })
+    this.loadDatapointsToState(opsWeb, this.props.match.params.id, viewId)
+  }
+
   public createPayload(datapoints: any[]) {
     const arrayToObject = (array: any[], keyField: any) =>
       array.reduce((obj, item) => {
@@ -223,24 +264,30 @@ class ListHome extends React.Component<IAppProps, IAppState> {
   }
 
   public submitRecord() {
-    const web = new Web(endpoint + "/operations");
     // add an item to the list if there is an
     // internet connection, otherwise alert the user
     if (navigator.onLine) {
-      web.lists
-      .getById(this.props.match.params.id)
-      .items.add(this.createPayload(this.state.datapoints))
-      .then((iar: ItemAddResult) => {
-        console.log(iar);
-      });
-    this.props.history.push("/");
+      opsWeb.lists
+        .getById(this.props.match.params.id)
+        .items.add(this.createPayload(this.state.datapoints))
+        .then((iar: ItemAddResult) => {
+          console.log(iar);
+        });
+      this.props.history.push("/");
     } else {
-      Alert.warning("You don't have an internet connection. Please go get one.", { position: "top" })
+      Alert.warning(
+        "You don't have an internet connection. Please go get one.",
+        { position: "top" }
+      );
     }
   }
 
   public refreshPage() {
-    location.reload()
+    location.reload();
+  }
+
+  public handleButtonClickem() {
+    this.newView("5bf68c7a-b494-4487-b97f-74335c6ed8ac")
   }
 
   public render() {
@@ -252,6 +299,7 @@ class ListHome extends React.Component<IAppProps, IAppState> {
           user={this.state.curUser.userName}
           logoutAction={this.onLogOut}
         />
+        {/* <button onClick={this.handleButtonClickem}>Click me</button> */}
         {this.state.loading ? (
           <h1>Loading...</h1>
         ) : (
@@ -266,7 +314,13 @@ class ListHome extends React.Component<IAppProps, IAppState> {
               .sort((a: any, b: any) => a.order - b.order)}
           />
         )}
-        {!this.state.loading && <ButtonBar submitEnabled={this.state.formIsValid} actionReset={this.refreshPage} actionSubmit={this.submitRecord} />}
+        {!this.state.loading && (
+          <ButtonBar
+            submitEnabled={this.state.formIsValid}
+            actionReset={this.refreshPage}
+            actionSubmit={this.submitRecord}
+          />
+        )}
       </Parent>
     );
   }
